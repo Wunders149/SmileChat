0bd799a1a1a29e614429effeea35cadb2eaebc34
wunders149 <razafimahefaphilibert7@gmail.com>
2025-01-16 10:01:28 +0300

    Tailwindcss Setup done!

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+
